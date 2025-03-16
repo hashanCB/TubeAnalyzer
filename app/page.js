e8ef6d3b77e8a07b11c7acc44bf075b00f9505c1
chanaka <hashan.chanaka96@gmail.com>
2025-03-16 17:23:53 +0000

    feat: develop landing page with YouTube video sentiment analysis
    - Added landing page with input field to navigate to video analysis page
    - Integrated YouTube API to fetch real-time video comments
    - Implemented sentiment analysis using \`sentiment\` library to analyze comments
    - Displayed suitable emoji based on overall sentiment of video comments
    - Added Lottie animations for visual feedback on sentiment results
    - Improved UI/UX with responsive design and smooth transitions

--- app/page.js
+++ app/page.js
@@ -7,20 +7,10 @@ import YouTubeDownloader from "./youtube-downloader";
 
 
 export default function Home() {
-  const [yurl,setyulr] = useState("")
-  const [trimvalu , settrim] = useState("")
 
-  useEffect(()=>{
-    let temptrimp = yurl.split("")
-    let newarry = temptrimp.slice(temptrimp.indexOf("=")+1).join("")
-    console.log(newarry)
-    settrim(newarry)
-  
-  },[yurl])
   return (
-   <div>
-
-    <YouTubeDownloader setyulr={setyulr} yurl={yurl}  trimvalu={trimvalu}/>
+   <div className="h-screen max-w-screen" >
+    <YouTubeDownloader />
    </div>
   );
 }
--- app/youtube-downloader.jsx
+++ app/youtube-downloader.jsx
@@ -3,42 +3,21 @@ import {  Link2 } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import React, { useEffect, useState, useTransition } from 'react'
-import { getyoutubevideo } from './api/youtube'
-import Sentiment from 'sentiment'
 
 
-import Very_Positivesvg from "./anime/smiling-face.json"; 
-import Positive from "./anime/winking-face.json"; 
-import Mild_Positive from "./anime/lottie.json"; 
-import Neutral from "./anime/neutral.json"; 
-import Mild_Negative from "./anime/slighty.json"; 
-import Negative from "./anime/nagative.json"; 
-import Very_Negative from "./anime/distraught.json"; 
 import Image from "next/image"
+import { useRouter } from "next/navigation"
 
-const sentimentToEmoji = {
-    "Very_Positive": Very_Positivesvg,
-    "Positive": Positive,
-    "Mild_Positive": Mild_Positive,
-    "Neutral": Neutral,
-    "Mild_Negative": Mild_Negative,
-    "Negative": Negative,
-    "Very_Negative": Very_Negative,
-}
+
 
 
 export default function YouTubeDownloader() {
-    const [videoscom , setvideocom] = useState([])
-    const [reaction,setReaction] = useState("")
-    const [finalmood,setFinalmood] = useState()
-    const [isfetch,setIsfetch] = useState(true)
-
     //genaral code
     const [yurl,setyulr] = useState("")
-    const [trimvalu , settrim] = useState("")
+   
     const [error,setError] = useState("")
     const [buttonActive,setbuttonActive] = useState(true)
-  
+    const route = useRouter()
 
 
     const useronChange = (data) => {
@@ -58,82 +37,11 @@ export default function YouTubeDownloader() {
       let temptrimp = yurl.split("")
       let newarry = temptrimp.slice(temptrimp.indexOf("=")+1).join("")
       console.log(newarry)
-      settrim(newarry)
+      route.push(`/${newarry}`)
+     
     }
 
-    //analazing part
-    
-    const sentimat = new Sentiment()
-    
-    //1st run
-    useEffect(()=>{
-      console.log(trimvalu)
-      const getvideo = async () =>{
-      const respoens = await getyoutubevideo(trimvalu)
-      setvideocom(respoens)
-      setIsfetch(false)
-      }
-       getvideo()
-    },[trimvalu])
-    
-    //count moods
-    useEffect(()=>{
-      let tempvalues = {
-          "Very_Positive" : 0,
-           "Positive":0,
-           "Mild_Positive" : 0,
-           "Neutral":0,
-           "Mild_Negative" : 0,
-           "Negative" : 0,
-           "Very_Negative":0
-      }
-      if (Array.isArray(videoscom))  {
-      videoscom.forEach((element,index) => {
-          
-          const result =  sentimat.analyze(element.text)
-          if (result.score >= 10) {
-             
-              tempvalues.Very_Positive = tempvalues.Very_Positive +1
-             // console.log("Very Positive");
-          } else if (result.score >= 5) {
-              tempvalues.Positive = tempvalues.Positive +1
-             // console.log("Positive");
-          } else if (result.score >= 1) {
-             // console.log("Mild Positive");
-             tempvalues.Mild_Positive = tempvalues.Mild_Positive +1
-          } else if (result.score === 0) {
-              tempvalues.Neutral = tempvalues.Neutral +1
-              //console.log("Neutral");
-          } else if (result.score >= -4) {
-              tempvalues.Mild_Negative = tempvalues.Mild_Negative +1
-            //  console.log("Mild Negative");
-          } else if (result.score >= -10) {
-              tempvalues.Negative = tempvalues.Negative +1
-             // console.log("Negative");
-          } else {
-              tempvalues.Very_Negative = tempvalues.Very_Negative +1
-            //  console.log("Very Negative");
-          }
-          
-        });
-      }
-    
-        setReaction(tempvalues)
-    },[videoscom])
-    
-    
-    //find reaction face
-    useEffect(()=>{
-        const analayze = () =>{
-        let convateraay = Object.entries(reaction)
-        let filterarray = convateraay.sort((a,b)=>b[1] - a[1])
-        let emoji = sentimentToEmoji[filterarray[0][0]]  // sentimentToEmoji
-        setFinalmood(emoji)
-        }
-        reaction ? analayze() :[]
-    },[reaction])
-    
-    
+ 
 
 
   return (
