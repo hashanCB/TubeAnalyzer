3679c020693fd4ca0f0b070e879101fdb8eca19f
chanaka <hashan.chanaka96@gmail.com>
2025-03-16 00:32:16 +0000

    Docker file Added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+        images: {
+            remotePatterns: [
+                {
+                  protocol: "https",
+                  hostname: "ferf1mheo22r9ira.public.blob.vercel-storage.com",
+                  pathname: "/**", // Allows all images in the storage
+                },
+              ],
+     
+        },
+      }
+
 
 export default nextConfig;
